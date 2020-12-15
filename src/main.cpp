d6e298ec7e41a2a8a007a4d8f8b550637fc4cc5b
wizche <sergio.paganoni@gmail.com>
2020-12-15 22:57:42 +0100

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,6 @@
 #include <stdexcept>
 #include <SD.h>
 
-template <typename... Args>
-std::string string_format(const std::string &format, Args... args)
-{
-  int size = snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
-  if (size <= 0)
-  {
-    throw std::runtime_error("Error during formatting.");
-  }
-  std::unique_ptr<char[]> buf(new char[size]);
-  snprintf(buf.get(), size, format.c_str(), args...);
-  return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
-}
-
 M5EPD_Canvas canvas(&M5.EPD);
 static int counter = 0;
 
@@ -49,7 +36,5 @@ void setup()
 
 void loop()
 {
-  Serial.printf("Update %d\n", counter);
-  counter += 1;
-  delay(1000);
+  delay(100);
 }
